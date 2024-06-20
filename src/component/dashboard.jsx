091c0aaad593f6d706e1fe0f094269f39194709d
haverwoods <shresthasamrat307@gmail.com>
2024-06-20 15:17:45 +0545

    dashboard base layout created

--- src/component/dashboard.jsx
+++ src/component/dashboard.jsx
@@ -4,14 +4,14 @@ import Sidebar from './sidbar';
 
 export const Dashboard = () => {
   return (
-    <div className='grid grid-cols-10 gap-0'>
-      <div className='col-start-1 col-end-3 '>
+    <div className='grid grid-cols-7 gap-0 bg-slate-900'>
+      <div className='grid col-span-1 '>
       <Sidebar />
       </div>
    
   
     {/* <div className='ml-56 h-screen bg-slate-900 text-white'> */}
-    <div className='col-start-2 col-end-11 h-screen bg-slate-900 text-white'>
+    <div className='col-start-2 col-end-11 h-screen ml-7 mt-5 text-white'>
     <h1> this is dashboard for home </h1>
     <h1> this is dashboard for home</h1>
     <h1> this is dashboard for home</h1>
--- src/component/sidbar.jsx
+++ src/component/sidbar.jsx
@@ -3,7 +3,7 @@ import { GiAutoRepair } from "react-icons/gi";
 
 export const Sidebar = () => {
   return (
-    <div className='fixed h-screen w-52  md:overflow-hidden overflow-auto md:hover:overflow-auto pb-10 color: bg-slate-500'>
+    <div className=' h-screen   pb-10 color: bg-slate-500'>
     <div className=''>    
     <div className='item-center mt-3 ml-3 gap-1 flex text-xl font-semibold'>
     <GiAutoRepair className='h-8 w-8'/>
@@ -11,12 +11,13 @@ export const Sidebar = () => {
        repair hub
     </span>    
        </div>
-       <div className='fixed mt-7 ml-3 left-0 m-0 flex flex-col text-white uppercase gap-3 hover:bg-black'>
-    <i>home</i>
-    <i>repair request</i>
-    <i>knowledge base</i>
-    <i>notification</i>
-    <i>setting</i>
+       <div className='fixed mt-7 ml-3 left-0 m-0 flex flex-col  gap-3 '>
+         <a href='' className='hover:bg-black text-white uppercase'>home</a>
+         <a href=''className='hover:bg-black text-white uppercase'>repair request</a>
+         <a href=''className='hover:bg-black text-white uppercase'>knowledge base</a>
+         <a href=''className='hover:bg-black text-white uppercase'>notification</a>
+         <a href=''className='hover:bg-black text-white uppercase'>setting</a>
+       
        </div>
     </div>
     </div>
