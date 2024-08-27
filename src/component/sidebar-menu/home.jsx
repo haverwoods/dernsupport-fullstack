e8c8f246dd6ee209797d06df1a5f87b88dfc9b1d
haverwoods <shresthasamrat307@gmail.com>
2024-08-27 12:47:49 +0545

    dropzone optimized

--- src/component/sidebar-menu/home.jsx
+++ src/component/sidebar-menu/home.jsx
@@ -1,13 +1,14 @@
 import React from "react";
 import Layout from "../layout/layout";
 
+
 export const Home = () => {
   return (
     <Layout>
       <div className="bg-gray-100">
         <div className="absolute h-screen w-auto min ml-5 mr-7 mt-5 text-black">
           <h1 className="text-2xl mt-2 mx-12 uppercase">
-            homepage is under maintainance
+            hompage is under maintainance
           </h1>
         </div>
       </div>
--- src/component/sidebar-menu/repairrequest.jsx
+++ src/component/sidebar-menu/repairrequest.jsx
@@ -1,84 +1,122 @@
-import React from "react";
+import React, { useState } from "react";
 import { MdOutlineDevices, MdOutlineDriveFolderUpload } from "react-icons/md";
 import Layout from "../layout/layout";
 import { IoCloudUploadOutline } from "react-icons/io5";
 
 export const Repairrequest = () => {
+  const [selectedFiles, setSelectedFiles] = useState([]);
+
+  const handleFileChange = (event) => {
+    // Convert the FileList object to an array
+    const files = Array.from(event.target.files);
+    // Update the state with the selected files
+    setSelectedFiles(files);
+  };
+
   return (
     <Layout>
-      <div className=" bg-gray-500">
-        <div className=" absolute h-screen w-auto  mt-1 text-black ">
-          <h1 className=" mt-0 mx-72 underline uppercase">
-            Repair request
-          </h1>
-          <div className=" bg-gray-300 h-auto w-96 my-2 mx-48  backdrop-blur-sm bg-opacity-50 border-b-2 rounded-lg ">
-            <form action="">
-              <div className=" ml-1 mr-5 mt-0   font-semibold">
-                <h1 className=" my-1 ml-5 uppercase">enter date</h1>
-                <input
-                  type="date"
-                  className="input-date bg-gray-200 outline-none border-none w-auto py-2 px-4  mx-5 mt-1 rounded-lg "
-                  required
-                />
-              </div>
-
-              <div className=" mx-5 mt-2  gap-1 font-semibold">
-                <div className="flex gap-3">
-                  <h1 className=" mt-2 my-1 uppercase">
-                    enter device name
-                  </h1>
-                  <MdOutlineDevices className="h-8 w-8 mt-2" />
+      <div className="">
+        <div className=" bg-gray-500">
+          <div className=" absolute h-screen w-auto  mt-1 text-black ">
+            <h1 className=" mt-0 mx-72 underline uppercase">Repair request</h1>
+            <div className=" bg-gray-300 h-auto w-96 my-2 mx-48  backdrop-blur-sm bg-opacity-50 border-b-2 rounded-lg ">
+              <form action="">
+                <div className=" ml-1 mr-5 mt-0   font-semibold">
+                  <h1 className=" my-1 ml-5 uppercase">enter date</h1>
+                  <input
+                    type="date"
+                    className="input-date bg-gray-200 outline-none border-none w-auto py-2 px-4  mx-5 mt-1 rounded-lg "
+                    required
+                  />
                 </div>
-                <input
-                  type="text"
-                  className="block w-1/2 py-2 text-sm bg-white border-b-2 my-1 appearance-none text-black rounded-lg"
-                  placeholder="enter device name"
-                  required
-                />
-              </div>
 
-              <div className="  input-box mx-5 mt-3  gap-1  font-semibold">
-                <div className="flex gap-3">
-                  <h1 className=" mt-2 my-1 uppercase">enter issue</h1>
-                  <MdOutlineDevices className="h-8 w-8 mt-2" />
+                <div className=" mx-5 mt-2  gap-1 font-semibold">
+                  <div className="flex gap-3">
+                    <h1 className=" mt-2 my-1 uppercase">enter device name</h1>
+                    <MdOutlineDevices className="h-8 w-8 mt-2" />
+                  </div>
+                  <input
+                    type="text"
+                    className="block w-1/2 py-2 text-sm bg-white border-b-2 my-1 appearance-none text-black rounded-lg"
+                    placeholder="enter device name"
+                    required
+                  />
                 </div>
-                <input
-                  type="text"
-                  className="block w-full max-w-lg h-full max-h-80  py-2 text-sm bg-white border-b-2 my-5 appearance-none text-black rounded-lg"
-                  placeholder="enter your issue"
-                  required
-                />
-              </div>
 
-              <div className="  input-box mx-5 mt-3  gap-1  text-xl font-semibold">
-                <div className="flex gap-3">
-                  <h1 className="text-xl mt-2 my-1 uppercase">upload pic</h1>
+                <div className="  input-box mx-5 mt-3  gap-1  font-semibold">
+                  <div className="flex gap-3">
+                    <h1 className=" mt-2 my-1 uppercase">enter issue</h1>
+                    <MdOutlineDevices className="h-8 w-8 mt-2" />
+                  </div>
+                  <input
+                    type="text"
+                    className="block w-full max-w-lg h-full max-h-80  py-2 text-sm bg-white border-b-2 my-5 appearance-none text-black rounded-lg"
+                    placeholder="enter your issue"
+                    required
+                  />
                 </div>
-                {/* //add drop zone */}
-                <div class="flex items-center justify-center w-full">
-                  <label
-                    for="dropzone-file"
-                    class="flex flex-col items-center justify-center w-full h-44 border-2 border-gray-300 border-dashed rounded-lg cursor-pointer bg-gray-50 dark:hover:bg-bray-800 dark:bg-gray-700 hover:bg-gray-100"
-                  >
-                    <div class="flex flex-col items-center justify-center pt-5 pb-6">          
-                      <IoCloudUploadOutline className="w-10 h-10 mb-3 text-gray-400"/>
-                      <p class="mb-2 text-sm text-gray-500 dark:text-gray-400">
-                        <span class="font-semibold">Click to upload</span> or
-                        drag and drop
-                      </p>
-                      {/* <p class="text-xs text-gray-500 dark:text-gray-400">
+
+                <div className="  input-box mx-5 mt-3  gap-1  text-xl font-semibold">
+                  <div className="flex gap-3">
+                    <h1 className="text-xl mt-2 my-1 uppercase">upload pic</h1>
+                  </div>
+                  {/* //add drop zone */}
+                  <div class="flex items-center justify-center w-full">
+                    {/* //label act as a cilckable area for file input */}
+                    <label
+                      for="dropzone-file"
+                      class="flex flex-col items-center justify-center w-full h-44 border-2 border-gray-300 border-dashed rounded-lg cursor-pointer bg-gray-50 dark:hover:bg-bray-800 dark:bg-gray-700 hover:bg-gray-100"
+                    >
+                      <div class="flex flex-col items-center justify-center pt-5 pb-6">
+                        <IoCloudUploadOutline className="w-10 h-10 mb-3 text-gray-400" />
+                        <p class="mb-2 text-sm text-gray-500 dark:text-gray-400">
+                          <span class="font-semibold">Click to upload</span> or
+                          drag and drop
+                        </p>
+                        <p class="text-xs text-gray-500 dark:text-gray-400">
                         SVG, PNG, JPG or GIF (MAX. 800x400px)
-                      </p> */}
-                      {/* //use array to save the selected data and display that data */}
-                    </div>
-                    <input id="dropzone-file" type="file" class="hidden" />
-                  </label>
+                      </p> 
+                        {/* {/* //use array to save the selected data and display that data */}
+                      </div>
+                      <input
+                        id="dropzone-file"
+                        type="file"
+                        class="hidden"
+                        onChange={handleFileChange}
+                        multiple
+                      />
+                    </label>
+                  </div>
+                  <div className="">
+                    {selectedFiles.length > 0 && (
+                      <ul>
+                        {/* Map through the selected files and display each file's name */}
+                        {selectedFiles.map((file, index) => (
+                          <li
+                            key={index}
+                            className="text-sm text-gray-500 dark:text-gray-400"
+                          >
+                            <span className="block w-32 truncate text-center">
+                              {file.name} {/* Display the name of the file */}
+                            </span>
+                            {file.type.startsWith("image/") && (
+                              <img
+                                src={URL.createObjectURL(file)} // Create a URL for the image preview
+                                alt={file.name}
+                                className="mt-2 h-20 w- object-cover rounded-lg"
+                              />
+                            )}
+                          </li>
+                        ))}
+                      </ul>
+                    )}
+                  </div>
                 </div>
-              </div>
-              <div className="button bg-blue-500 w-32 hover:bg-blue-700 text-white font-bold ml-7 mb-3 mt-5 py-2 px-4 rounded-full">
-                submit
-              </div>
-            </form>
+                <div className="button bg-blue-500 w-32 hover:bg-blue-700 text-white font-bold ml-7 mb-3 mt-5 py-2 px-4 rounded-full">
+                  submit
+                </div>
+              </form>
+            </div>
           </div>
         </div>
       </div>
